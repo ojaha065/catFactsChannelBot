f3e6971f3d466e0ce2924aeacbed946e9c3f7dc9
Jani Haiko <haiko.jani@gmail.com>
2019-11-12 20:35:07 +0200

    Now sends pictures with the facts

--- server.js
+++ server.js
@@ -6,11 +6,13 @@ if(process.env.NODE_ENV !== "production"){
     require("dotenv").config();
 }
 
+const fs = require("fs");
 const request = require("request-promise");
 const Telegraf = require("telegraf");
 const Telegram = require("telegraf/telegram");
 
-const catFactsAPIUrl = "https://catfact.ninja/";
+const catFactsAPIUrl = "https://catfact.ninja";
+const CATAAS_APIUrl = "https://cataas.com";
 
 const API_TOKEN = process.env.API_TOKEN;
 if(!API_TOKEN){
@@ -57,6 +59,7 @@ if(!process.env.NO_WEBHOOK_NEEDED){
 }
 
 let running = true;
+let currentBreed;
 let loop;
 startLoop();
 
@@ -88,17 +91,64 @@ bot.command("/stop",(ctx) => {
 
 bot.command("/post",async (ctx) => {
     if(authUser(ctx.update.message.from.id,ctx.update.message.from.username)){
-        ctx.reply("Sending a new cat fact now...");
-        const fact = await getCatFact();
-        ctx.telegram.sendMessage(channelId,fact);
+        try{
+            await ctx.reply("Sending a new cat fact now...");
+            const fact = await getCatFact();
+            const imageFileName = await getRandomCatPicture();
+            if(imageFileName){
+                await telegram.sendPhoto(channelId,{
+                    source: fs.readFileSync(`./${imageFileName}`)
+                });
+                telegram.sendMessage(channelId,fact);
+            }
+            else{
+                console.warn("It seems that getting the image failed");
+                telegram.sendMessage(PRIVATE_CHAT_ID,"Getting image from CATAAS failed! Please check the server console for more information.");
+                telegram.sendMessage(channelId,fact);
+            }
+        }
+        catch(error){
+            console.error(error);
+            ctx.reply("Failed! Please check the server console for more information.");
+        }
     }
     else{
         ctx.reply("Sorry, you are not allowed to use that command right now.");
     }
 });
+bot.command("/breed",async (ctx) => {
+    if(authUser(ctx.update.message.from.id,ctx.update.message.from.username)){
+        try{
+            const response = await request.get({
+                uri: `${catFactsAPIUrl}/breeds?page=${Math.floor(Math.random() * 4) + 1}`,
+                resolveWithFullResponse: true
+            });
+            if(response.statusCode === 200){
+                const breeds = JSON.parse(response.body).data;
+                currentBreed = breeds[Math.floor(Math.random() * breeds.length - 1)];
+                await telegram.sendMessage(channelId,`Meow there! I hereby declare today as a day of the *${currentBreed.breed}*. ${currentBreed.breed} is a ${Math.random() < 0.5 ? "beautiful" : "lovely"} breed from ${currentBreed.country}. ${currentBreed.breed} cats usually have a ${currentBreed.coat.toLowerCase()} coat${currentBreed.pattern ? " and a " + currentBreed.pattern.toLowerCase() + " pattern." : "."}`,{
+                    parse_mode: "Markdown"
+                });
+                ctx.reply("Posted!");
+            }
+            else{
+                console.warn(`Cat Facts API HTTP response code was ${response.statusCode}`);
+                telegram.sendMessage(PRIVATE_CHAT_ID,`Cat Facts API HTTP response code was ${response.statusCode}`);
+            }
+        }
+        catch(error){
+            console.error(error);
+            ctx.reply("Failed! Please check the server console for more information.");
+        }
+    }
+    else{
+        ctx.reply("I'm meow sorry, but you are not my master.");
+    }
+});
 
 bot.command("/ping",(ctx) => {
     //console.log(ctx.update);
+    getRandomCatPicture()
     ctx.reply("pong!");
 });
 bot.command("/fact",async (ctx) => {
@@ -136,7 +186,6 @@ async function getCatFact(){
             resolveWithFullResponse: true
         });
         if(response.statusCode === 200){
-            JSON.parse(response.body);
             return JSON.parse(response.body).fact;
         }
         else{
@@ -149,12 +198,51 @@ async function getCatFact(){
         return null;
     }
 }
+async function getRandomCatPicture(){
+    try{
+        const image = await request.get(`${CATAAS_APIUrl}/cat`,{
+            encoding: "binary"
+        });
+        const filename = `CATAAS_${new Date().getTime()}`;
+        fs.writeFileSync(filename,image,"binary");
+        setTimeout(() => {
+            try{
+                fs.unlinkSync(`./${filename}`);
+            }
+            catch(error){
+                console.warn(error);
+            }
+        },60000);
+        return filename;
+    }
+    catch(error){
+        console.error(error);
+        return null;
+    }
+}
 
 function startLoop(){
     loop = setInterval(async () => {
         if(running){
-            const fact = await getCatFact();
-            telegram.sendMessage(channelId,fact);
+            try{
+                const fact = await getCatFact();
+                const imageFileName = await getRandomCatPicture();
+                if(imageFileName){
+                    await telegram.sendPhoto(channelId,{
+                        source: fs.readFileSync(`./${imageFileName}`)
+                    });
+                    telegram.sendMessage(channelId,fact);
+                }
+                else{
+                    console.warn("It seems that getting the image failed");
+                    telegram.sendMessage(PRIVATE_CHAT_ID,"Getting image from CATAAS failed! Please check the server console for more information.");
+                    telegram.sendMessage(channelId,fact);
+                }
+            }
+            catch(error){
+                console.error(error);
+                ctx.reply("Interval failed! Please check the server console for more information.");
+            }
         }
         else{
             clearInterval(loop);
